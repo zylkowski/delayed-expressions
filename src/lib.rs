583245132a63612576db72ec2176e0434d63ed59
zylkowski_a <arkadiuszzylkowski@gmail.com>
2021-12-09 13:13:45 +0100

    Added more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,144 +1,15 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 use delayed_expressions_derive::*;
-use std::{
-    fmt::Debug,
-    ops::{Add, Div, Mul, Sub},
-};
-
-trait Operand<T>: Debug {
-    fn get_value(self: Box<Self>) -> T;
-}
-
-impl<T: Debug> Operand<T> for T {
-    fn get_value(self: Box<Self>) -> T {
-        *self
-    }
-}
-
-trait Op<T>: Debug {
-    fn execute(&self, lhs: T, rhs: T) -> T;
-}
-
-#[derive(Debug)]
-struct AddOp {}
-impl<T: Add<Output = T>> Op<T> for AddOp {
-    fn execute(&self, lhs: T, rhs: T) -> T {
-        lhs + rhs
-    }
-}
-
-#[derive(Debug)]
-struct SubOp {}
-impl<T: Sub<Output = T>> Op<T> for SubOp {
-    fn execute(&self, lhs: T, rhs: T) -> T {
-        lhs - rhs
-    }
-}
-
-#[derive(Debug)]
-struct MulOp {}
-impl<T: Mul<Output = T>> Op<T> for MulOp {
-    fn execute(&self, lhs: T, rhs: T) -> T {
-        lhs * rhs
-    }
-}
-
-#[derive(Debug)]
-struct DivOp {}
-impl<T: Div<Output = T>> Op<T> for DivOp {
-    fn execute(&self, lhs: T, rhs: T) -> T {
-        lhs / rhs
-    }
-}
-
-#[derive(Debug)]
-enum NodeType<T> {
-    Operation(Box<dyn Op<T>>),
-    Val(Box<dyn Operand<T>>),
-}
-
-trait ExpressionExecute<T> {
-    fn execute(self) -> T;
-}
-
-type ChildNode<T> = Option<Box<BTNode<T>>>;
-#[derive(Debug)]
-struct BTNode<T> {
-    left: ChildNode<T>,
-    right: ChildNode<T>,
-    op: NodeType<T>,
-}
-
-impl<T> BTNode<T> {
-    fn new(val: impl Operand<T> + 'static) -> Self {
-        BTNode {
-            left: None,
-            right: None,
-            op: NodeType::Val(Box::new(val)),
-        }
-    }
-
-    fn op(operator: impl Op<T> + 'static, lhs: BTNode<T>, rhs: BTNode<T>) -> Self {
-        BTNode {
-            left: Some(Box::new(lhs)),
-            right: Some(Box::new(rhs)),
-            op: NodeType::Operation(Box::new(operator)),
-        }
-    }
-}
-
-impl<T: Add<Output = T>> Add for BTNode<T> {
-    type Output = BTNode<T>;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        BTNode {
-            left: Some(Box::new(self)),
-            right: Some(Box::new(rhs)),
-            op: NodeType::Operation(Box::new(AddOp {})),
-        }
-    }
-}
-
-impl<T: Sub<Output = T>> Sub for BTNode<T> {
-    type Output = BTNode<T>;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        BTNode {
-            left: Some(Box::new(self)),
-            right: Some(Box::new(rhs)),
-            op: NodeType::Operation(Box::new(SubOp {})),
-        }
-    }
-}
-
-impl<T: Mul<Output = T>> Mul for BTNode<T> {
-    type Output = BTNode<T>;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        BTNode {
-            left: Some(Box::new(self)),
-            right: Some(Box::new(rhs)),
-            op: NodeType::Operation(Box::new(MulOp {})),
-        }
-    }
-}
-
-impl<T: Div<Output = T>> Div for BTNode<T> {
-    type Output = BTNode<T>;
-
-    fn div(self, rhs: Self) -> Self::Output {
-        BTNode {
-            left: Some(Box::new(self)),
-            right: Some(Box::new(rhs)),
-            op: NodeType::Operation(Box::new(DivOp {})),
-        }
-    }
-}
+pub mod operators;
+use operators::*;
+pub mod btree;
+use btree::*;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ops::{Add, Mul};
 
     #[default_execute]
     type Integer = BTNode<i32>;
@@ -153,5 +24,23 @@ mod tests {
 
         assert!(a_result == b_result);
         assert!(a_result == 12);
+    }
+
+    #[test]
+    fn custom_operator() {
+        #[derive(Debug)]
+        struct AddNTimesOp {
+            n: usize,
+        }
+        impl<T: Add<Output = T> + Copy> Op<T> for AddNTimesOp {
+            fn execute(&self, lhs: T, rhs: T) -> T {
+                (0..self.n).fold(lhs, |lhs, _| lhs + rhs)
+            }
+        }
+
+        let a = Integer::op(AddNTimesOp { n: 3 }, Integer::new(5), Integer::new(2));
+        let a_result = a.execute();
+
+        assert!(a_result == 11);
     }
 }
